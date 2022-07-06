bbacbe67e0d2b430974c16abcd91b3a299ff56f2
Tomas Riquelme <tomas.riquelmemillar@cencosud.cl>
2022-07-06 18:24:27 -0400

    add test

---
+++ src/test/java/mingeso/configserver/ConfigServerControllerTest.java
@@ -0,0 +1,16 @@
+package mingeso.configserver;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ConfigServerControllerTest {
+
+    @Test
+    public void helloWorld() {
+        Integer n = 2;
+        assertEquals(n, 2);
+    }
+}
