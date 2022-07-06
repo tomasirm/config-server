f06b87c209ede45e1f32a20d01e6240a1c8264c6
Tomas Riquelme <tomas.riquelmemillar@cencosud.cl>
2022-07-05 23:13:25 -0400

    subida

---
+++ src/test/java/mingeso/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package mingeso.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
